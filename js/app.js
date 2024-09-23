19c0fa4cce401abc77810bf5e9468fb29a5a29f9
Jubayer husain <jubayerhusai360@gmail.com>
2024-09-23 11:06:40 +0600

    utilitise file done with some update

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
-// alert("Your file is setup done")
+// get the Noakhali donationBtn 
+const noakhaliDonationBtn = document.getElementById('noakhali-donate-btn');
+noakhaliDonationBtn.addEventListener('click', function () {
+    // get the main balance 
+    const mainBalance = getTheElementId('balance');
+    // // get the noakhaliDonateInput 
+    const noakhaliDonateInput = getInputFieldById('noakhali-donate-input');
+    const newBalance = mainBalance - noakhaliDonateInput;
+    document.getElementById('balance').innerText = newBalance;
+    document.getElementById('noakhali-donate-balance').innerText = noakhaliDonateInput;
+})
+
+// get the feni donationBtn 
+const feniDonationBtn = document.getElementById('feni-donate-btn');
+feniDonationBtn.addEventListener('click', function () {
+    // get the main balane 
+    const mainBalance = getTheElementId('balance');
+    const feniDonateInput = getInputFieldById('feni-donate-input');
+    const newBalance = mainBalance - feniDonateInput;
+    // added the new balance 
+    document.getElementById('balance').innerText = newBalance;
+    // // get the  noakhali-donate-balance 
+    document.getElementById('feni-donate-balance').innerText = feniDonateInput;
+})
+// get the Quata donationBtn 
+const quataDonationBtn = document.getElementById('quata-donate-btn');
+quataDonationBtn.addEventListener('click', function () {
+    // get the main balane 
+    const mainBalance = getTheElementId('balance');
+    const quataDonateInput = getInputFieldById('quata-donate-input');
+    const newBalance = mainBalance - quataDonateInput;
+    // added the new balance 
+    document.getElementById('balance').innerText = newBalance;
+    // // get the  noakhali-donate-balance 
+    document.getElementById('quata-donate-balance').innerText = quataDonateInput;
+})
---
+++ js/utilitise.js
@@ -0,0 +1,8 @@
+function getInputFieldById(id) {
+    const inPutFieldValue =  parseFloat(document.getElementById(id).value);
+    return inPutFieldValue;
+}
+function getTheElementId(id){
+    const elementId = parseFloat(document.getElementById(id).innerText);
+    return elementId;
+}
